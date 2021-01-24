21f25468d5cfcf8be4cbc8f9e4094cec286373f4
Maxine Krebs <krebs.maxine@gmail.com>
2021-01-24 02:52:06 -0800

    Add support for movie files as well
    i realized that the only real difference between movie files or tv files
    or any arbitrary file is the pattern that you want to match against. so
    instead of having completly diffent code paths for different file
    types, i can basically do the same thing for all files and just change
    the pattern used based on type

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 - I should add a test suite to exercise various formats and update it as I come across new and exiting exceptions.
 - I'd like to generalize it a bit so its more widely applicable
-- similar to ^ I want to make it support movie titles as well
 
+Currently movie files need to be in this format `Title.Year.[...].mkv`
+And tv show files need to be of the format `Tite.SeasonEpisode.[...].mlv`
--- batch_renamer.py
+++ batch_renamer.py
@@ -12,25 +12,7 @@ def str2bool(v):
   else:
    raise argparse.ArgumentTypeError('Boolean value expected.')
 
-parser = argparse.ArgumentParser()
-parser.add_argument("directory")
-parser.add_argument("-y", type=str2bool, nargs='?',
-                      const=True, default=False,
-                      help="skip the checks per rename")
-args = parser.parse_args()
-
-directory = args.directory
-
-prog = re.compile("(^\S+)\.(S\d+e\d+)")
-
-for file in os.listdir(directory):
-  filename, file_extension = os.path.splitext(file)
-  show_name = prog.search(filename).group(0)
-  new_title = show_name + file_extension
-
-  current_file = directory + file
-  new_file = directory + new_title
-
+def rename_file(current_file, new_file):
   answer = ""
   if args.y == True:
     answer = "y"
@@ -41,10 +23,51 @@ for file in os.listdir(directory):
 
   if answer == "y":
     os.rename(current_file, new_file)
+    return
   elif answer == "q":
     print("Goodbye 👋")
     exit(0)
   else:
-    continue
+    return
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument("directory")
+parser.add_argument("type", choices=["movies", "shows"])
+parser.add_argument("-y", type=str2bool, nargs='?',
+                      const=True, default=False,
+                      help="skip the checks per rename")
+parser.add_argument("--dry-run", type=str2bool, nargs='?',
+                      const=True, default=False,
+                      help="dont actually write filename changes")
+args = parser.parse_args()
+
+directory = args.directory
+type = args.type
+
+def rename_matching(pattern):
+  prog = re.compile(pattern, flags=re.I)
+
+  for file in os.listdir(directory):
+    filename, file_extension = os.path.splitext(file)
+    matches = prog.search(filename)
+    if matches == None:
+      print("no matches found in file: {0}".format(filename))
+      continue
+
+    show_name = matches.group(0)
+    new_title = show_name + file_extension
+
+    current_file = directory + file
+    new_file = directory + new_title
+    if args.dry_run == True:
+      print("re-naming file: {0} to: {1}".format(current_file, new_file))
+    else:
+      rename_file(current_file, new_file)
+
+if type == "shows":
+  rename_matching("(^\S+)\.(s\d+e\d+)")
+elif type == "movies":
+  rename_matching("([\[a-zA-Z\]\.?]+)\.(\d+)")
+
+print("done! 🎉")
