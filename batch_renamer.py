b71dde352fbda9be0005d2f8e3e3bf7e4ba23d46
Maxine Krebs <krebs.maxine@gmail.com>
2021-01-24 03:04:25 -0800

    A bit more code organization

--- batch_renamer.py
+++ batch_renamer.py
@@ -2,6 +2,65 @@ import os
 import argparse
 import re
 
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("directory")
+  parser.add_argument("type", choices=["movies", "shows"])
+  parser.add_argument("-y", type=str2bool, nargs='?',
+                        const=True, default=False,
+                        help="skip the checks per rename")
+  parser.add_argument("--dry-run", type=str2bool, nargs='?',
+                        const=True, default=False,
+                        help="dont actually write filename changes")
+  args = parser.parse_args()
+  skip_confirm = args.y
+  dry_run = args.dry_run
+  directory = args.directory
+  type = args.type
+
+  if type == "shows":
+    rename_matching("(^\S+)\.(s\d+e\d+)", directory, skip_confirm, dry_run)
+  elif type == "movies":
+    rename_matching("([\[a-zA-Z\]\.?]+)\.(\d+)", directory, skip_confirm, dry_run)
+
+  print("done! 🎉")
+
+def rename_matching(pattern, directory, skip_confirm=False, dry_run=False):
+  prog = re.compile(pattern, flags=re.I)
+
+  for file in os.listdir(directory):
+    filename, file_extension = os.path.splitext(file)
+    matches = prog.search(filename)
+    if matches == None:
+      print("no matches found in filename: {0}".format(filename))
+      continue
+
+    show_name = matches.group(0)
+    new_title = show_name + file_extension
+
+    current_file = directory + file
+    new_file = directory + new_title
+    if dry_run == True:
+      print("{0} to: {1}".format(current_file, new_file))
+    else:
+      answer = ""
+
+      if skip_confirm == True:
+        answer = "y"
+
+      while answer not in ["y", "n", "q"]:
+        print("re-naming file: {0} to: {1}".format(current_file, new_file))
+        answer = input("👍? [(y)es/(n)o/(q)uit]").lower()
+
+      if answer == "y":
+        os.rename(current_file, new_file)
+        continue
+      elif answer == "q":
+        print("Goodbye 👋")
+        exit(0)
+      else:
+        continue
+
 def str2bool(v):
   if isinstance(v, bool):
     return v
@@ -12,62 +71,4 @@ def str2bool(v):
   else:
    raise argparse.ArgumentTypeError('Boolean value expected.')
 
-def rename_file(current_file, new_file):
-  answer = ""
-  if args.y == True:
-    answer = "y"
-
-  while answer not in ["y", "n", "q"]:
-    print("re-naming file: {0} to: {1}".format(current_file, new_file))
-    answer = input("👍? [(y)es/(n)o/(q)uit]").lower()
-
-  if answer == "y":
-    os.rename(current_file, new_file)
-    return
-  elif answer == "q":
-    print("Goodbye 👋")
-    exit(0)
-  else:
-    return
-
-
-parser = argparse.ArgumentParser()
-parser.add_argument("directory")
-parser.add_argument("type", choices=["movies", "shows"])
-parser.add_argument("-y", type=str2bool, nargs='?',
-                      const=True, default=False,
-                      help="skip the checks per rename")
-parser.add_argument("--dry-run", type=str2bool, nargs='?',
-                      const=True, default=False,
-                      help="dont actually write filename changes")
-args = parser.parse_args()
-
-directory = args.directory
-type = args.type
-
-def rename_matching(pattern):
-  prog = re.compile(pattern, flags=re.I)
-
-  for file in os.listdir(directory):
-    filename, file_extension = os.path.splitext(file)
-    matches = prog.search(filename)
-    if matches == None:
-      print("no matches found in file: {0}".format(filename))
-      continue
-
-    show_name = matches.group(0)
-    new_title = show_name + file_extension
-
-    current_file = directory + file
-    new_file = directory + new_title
-    if args.dry_run == True:
-      print("re-naming file: {0} to: {1}".format(current_file, new_file))
-    else:
-      rename_file(current_file, new_file)
-
-if type == "shows":
-  rename_matching("(^\S+)\.(s\d+e\d+)")
-elif type == "movies":
-  rename_matching("([\[a-zA-Z\]\.?]+)\.(\d+)")
-
-print("done! 🎉")
+main()
