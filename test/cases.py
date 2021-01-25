1ba0f14efdde31365ff01a2bf6733e1f91ef7ae2
Maxine Krebs <krebs.maxine@gmail.com>
2021-01-24 23:49:38 -0800

    add test cases file for future testing purposes

---
+++ test/cases.py
@@ -0,0 +1,18 @@
+# files names to be used as test cases
+
+movies = [
+  "Seberg.2019.1080p.WEBRip.x264.AAC5.1-[YTS.MX].mp4",
+  "But.I'm.A.Cheerleader.1999.1080p.WEBRip.x264-[YTS.AM].mp4",
+  "Happiest.Season.2020.1080p.WEBRip.x264.AAC5.1-[YTS.MX].mp4",
+  "Valhalla.Rising.2009.1080p.BrRip.x264.YIFY.mp4",
+  "Valhalla.Rising.2009.1080p.BrRip.x264.YIFY.srt",
+  "The.Secret.Of.Kells.2009.mp4"
+]
+
+
+shows = [
+  "The.Expanse.S05E07.Oyedeng.REPACK.1080p.AMZN.WEB-DL.DDP5.1.H.264-NTG.mkv",
+  "The.Expanse.s05e07.Oyedeng.REPACK.1080p.AMZN.WEB-DL.DDP5.1.H.264-NTG.mkv",
+  "Wandavision.S01e02.mp4",
+  "WandaVision.S01E03.1080p.DSNP.WEB-DL.DDP5.1.Atmos.H.264-CMRG.mkv"
+]
